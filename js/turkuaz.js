53bc6fcd2ca0616cf7d2dc7d6cc466bb47dbc4e6
Ömer Gülçiçek <iletisim@omergulcicek.com>
2017-02-19 23:00:52 +0300

    Gizle, Footer, İçerik
    Detaylı gizle sınıfları eklendi.
    Footer'a detay eklendi.
    Karışıklıkları önlemek için içerik sınıfları güncellendi.

--- js/turkuaz.js
+++ js/turkuaz.js
@@ -1,41 +1,12 @@
-/*!
- * Turkuaz Framework v1.0
- * www.turkuazcss.com
- * MIT Lisansi (https://raw.githubusercontent.com/TurkuazCss/Framework/master/LICENSE)
- */
-
-/*!
- *    Turkuaz Framework - JavaScript Kod Duzeni
- *
- *   1. Etiket
- *   2. Form
- *   3. Menu
- *   4. Medya
- *   5. Modal
- *
- */
-
-
-
-/* ============= 1. Etiket */
-
 $("a.etiket.kapat").click(function(e) {
     e.preventDefault();
 });
-
-$("a.etiket.kapat").append("<span class=kaldir>&times</span>")
-    .end()
-    .find("span.kaldir").click(function() {
-        $(this).parents("a.etiket.kapat").addClass("gizle");
-    });
-
-/* ============= 2. Form */
-
+$("a.etiket.kapat").append("<span class=kaldir>&times</span>").end().find("span.kaldir").click(function() {
+    $(this).parents("a.etiket.kapat").addClass("gizle");
+});
 $formEtiketleri = ".tr-input,.tr-search,.tr-checkbox,.tr-radio,.tr-textarea";
 $placeholder = ".tr-input,.tr-textarea";
-
 placeholder();
-
 $($formEtiketleri).focus(function() {
     var inputId = $(this).attr("id");
     $("body").find("[for='" + inputId + "']").addClass("dolu");
@@ -56,100 +27,72 @@ function placeholder() {
         }
     });
 }
-
 $(".tr-textarea").on("keydown focusout", function(e) {
     var t = $(this);
     var line = t.val().split("\n").length * 20;
     if (line > 100) {
         t.css("height", line);
-    }
-    else {
+    } else {
         t.css("height", 100);
     }
 });
-
-/* ============= 3. Menu */
-
-//Mobil Menu
-
 $("nav.mobil li ul, nav.menu li ul").parents("li").addClass("acilir");
-$("nav.mobil li.acilir ul, nav.menu li.acilir ul").hide();
+$("nav.mobil li.acilir ul").hide();
 $("nav.mobil").on("click", "li.acilir>a", function() {
     $(this).parents("li.acilir:first").find("ul:first").slideToggle();
 });
 $("body").on("click", ".menuac", function() {
-    $(this).animate({ opacity: "0" }, 10).parents("body").append("<div class='tr-karart menu'></div>").find("nav.mobil").scrollTop(0).animate({ left: "0" });
+    $(this).animate({
+        opacity: "0"
+    }, 10).parents("body").append("<div class='tr-karart menu'></div>").find("nav.mobil").scrollTop(0).animate({
+        left: "0"
+    });
 });
 $("body").on("click", ".tr-karart.menu", function() {
     $(this).remove();
-    $(".menuac").delay(400).animate({ opacity: "1" }, 250);
-    $("nav.mobil").animate({ left: "-250px" });
-})
-
-// Tab Menu
-;
+    $(".menuac").delay(400).animate({
+        opacity: "1"
+    }, 250);
+    $("nav.mobil").animate({
+        left: "-250px"
+    });
+});
 (function($) {
     $.fn.tab = function(ayarlar) {
         var obj = $.extend({
             "aktifSinifi": "aktif",
-            "icerikSinifi": "tr-tab-icerik",
+            "icerikSinifi": "tab-icerik",
             "aktifSekme": 1,
             "tema": ""
         }, ayarlar);
         return this.each(function() {
-            $(this).addClass("tr-tab " + obj.tema)
-                .find("nav a").eq(obj.aktifSekme - 1).addClass(obj.aktifSinifi)
-                .parents(".tr-tab").find("." + obj.icerikSinifi).addClass("tr-tab-icerik gizle")
-                .eq(obj.aktifSekme - 1).removeClass("gizle");
+            $(this).addClass("tr-tab " + obj.tema).find("nav a").eq(obj.aktifSekme - 1).addClass(obj.aktifSinifi).parents(".tr-tab").find("." + obj.icerikSinifi).addClass("tab-icerik gizle").eq(obj.aktifSekme - 1).removeClass("gizle");
             $(this).find("nav a").click(function() {
                 var index = $(this).index();
-                $(this).addClass(obj.aktifSinifi)
-                    .siblings().removeClass(obj.aktifSinifi)
-                    .parents(".tr-tab").find("." + obj.icerikSinifi).addClass("gizle")
-                    .eq(index).removeClass("gizle");
+                $(this).addClass(obj.aktifSinifi).siblings().removeClass(obj.aktifSinifi).parents(".tr-tab").find("." + obj.icerikSinifi).addClass("gizle").eq(index).removeClass("gizle");
             });
         });
     };
 })(jQuery);
-
 $(".tr-tab").tab();
-
-// Filtre Menu
 $(".tr-filtre [data-source=hepsi]").addClass("aktif");
 $(".tr-filtre").on("click", "nav a", function() {
     var source = $(this).data("source");
-    $(this).addClass("aktif")
-        .siblings().removeClass("aktif")
-        .end()
-        .parents(".tr-filtre").find(".tr.icerik").removeClass("gizle")
-        .not("[data-target^=" + source + "]").addClass("gizle");
+    $(this).addClass("aktif").siblings().removeClass("aktif").end().parents(".tr-filtre").find(".tr.icerik").removeClass("gizle").not("[data-target^=" + source + "]").addClass("gizle");
     if (source == "hepsi" || source == "hepsi aktif") {
         $(this).parents(".tr-filtre").find(".icerik").removeClass("gizle");
     }
 })
-
-// Akordiyon Menu
-$(".tr-akordiyon-icerik").hide();
+$(".akordiyon-icerik").hide();
 $(".tr-akordiyon > a.baslik").on("click", function() {
     var t = $(this);
     var i = t.closest(".tr-akordiyon").children("a.baslik").index(t);
-    if(t.hasClass("aktif")) {
-        t.removeClass("aktif")
-        .closest(".tr-akordiyon").children(".tr-akordiyon-icerik").slideUp(250);
-    }
-    else {
-        t.siblings().removeClass("aktif")
-        .end().addClass("aktif")
-        .closest(".tr-akordiyon").children(".tr-akordiyon-icerik").eq(i).slideDown(250)
-        .siblings(".tr-akordiyon-icerik").slideUp(250);
+    if (t.hasClass("aktif")) {
+        t.removeClass("aktif").closest(".tr-akordiyon").children(".akordiyon-icerik").slideUp(250);
+    } else {
+        t.siblings().removeClass("aktif").end().addClass("aktif").closest(".tr-akordiyon").children(".akordiyon-icerik").eq(i).slideDown(250).siblings(".akordiyon-icerik").slideUp(250);
     }
 })
-
-/* ============= 4. Medya */
-
-
-/* ============= 5. Modal */
-
 $("body").on("click", "a.modal", function(e) {
     e.preventDefault();
     var href = $(this).attr("href");
@@ -160,7 +103,6 @@ $("body").on("click", "a.modal", function(e) {
         $("body").append(overlay);
     }
 });
-
 $("body").on("click", "a.kapat,.tr-karart", function(e) {
     e.preventDefault();
     $(".tr-modal").removeClass("goster");
