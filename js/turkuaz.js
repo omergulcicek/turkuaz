84534d525d0fd10cb9ac14fb097b2fefa6d69c9f
Ömer Gülçiçek <iletisim@omergulcicek.com>
2017-03-07 01:12:08 +0300

    Select
    Select kodları tamamlandı.
    Tablo kodlarında sağ ve sol border'lar kaldırıldı.

--- js/turkuaz.js
+++ js/turkuaz.js
@@ -4,6 +4,42 @@ $("a.etiket.kapat").click(function(e) {
 $("a.etiket.kapat").append("<span class=kaldir>&times</span>").end().find("span.kaldir").click(function() {
     $(this).parents("a.etiket.kapat").addClass("gizle");
 });
+
+
+$("select.tr-select").each(function() {
+    var optionLength = $(this).children("option").length;
+    $(this).addClass("gizle").wrap("<div class='select'></div>").after("<div class='tr-select-secili'></div>");
+    var selectSecili = $(this).next("div.tr-select-secili");
+    selectSecili.text($(this).children("option").eq(0).text());
+    var list = $("<ul/>", {
+        "class": "tr-select"
+    }).insertAfter(selectSecili);
+    for (var i = 0; i < optionLength; i++) {
+        $("<li/>", {
+            text: $(this).children("option").eq(i).text(),
+            rel: $(this).children("option").eq(i).val()
+        }).appendTo(list);
+    }
+    var li = list.children("li");
+    selectSecili.click(function(e) {
+        e.stopPropagation();
+        $("div.selectSecili.aktif").each(function() {
+            $(this).removeClass("aktif").next("ul.tr-select").hide();
+        });
+        $(this).toggleClass("aktif").next("ul.tr-select").toggle();
+    });
+    li.click(function(e) {
+        e.stopPropagation();
+        selectSecili.text($(this).text()).removeClass("aktif");
+        $(this).val($(this).attr("rel"));
+        list.hide();
+    });
+    $(document).click(function() {
+        selectSecili.removeClass("aktif");
+        list.hide();
+    });
+});
+
 $formEtiketleri = ".tr-input,.tr-search,.tr-checkbox,.tr-radio,.tr-textarea";
 $placeholder = ".tr-input,.tr-textarea";
 placeholder();
@@ -29,11 +65,19 @@ function placeholder() {
 }
 $(".tr-textarea").on("keydown focusout", function(e) {
     var t = $(this);
+    var l = t.next("label.tr-label");
     var line = t.val().split("\n").length * 20;
     if (line > 100) {
         t.css("height", line);
+        l.attr('style', 'transform: translateY(-' + (line+30) + 'px) scale(.75) !important');
     } else {
         t.css("height", 100);
+        if (l.hasClass("dolu")) {
+            l.attr('style', 'transform: translateY(-130px) scale(.75) !important');
+        }
+        else {
+            l.attr('style', '');
+        }
     }
 });
 $("nav.mobil li ul, nav.menu li ul").parents("li").addClass("acilir");
@@ -44,18 +88,33 @@ $("nav.mobil").on("click", "li.acilir>a", function() {
 $("body").on("click", ".menuac", function() {
     $(this).animate({
         opacity: "0"
-    }, 10).parents("body").append("<div class='tr-karart menu'></div>").find("nav.mobil").scrollTop(0).animate({
-        left: "0"
-    });
+    }, 10).parents("body").append("<div class='tr-karart menu'></div>").find("nav.mobil").scrollTop(0);
+    if ($("nav.mobil").hasClass("sag")) {
+        $("nav.mobil").animate({
+            right: "0"
+        });
+    }
+    else {
+        $("nav.mobil").animate({
+            left: "0"
+        });
+    }
 });
 $("body").on("click", ".tr-karart.menu", function() {
     $(this).remove();
     $(".menuac").delay(400).animate({
         opacity: "1"
     }, 250);
-    $("nav.mobil").animate({
-        left: "-250px"
-    });
+    if ($("nav.mobil").hasClass("sag")) {
+        $("nav.mobil").animate({
+            right: "-250px"
+        });
+    }
+    else {
+        $("nav.mobil").animate({
+            left: "-250px"
+        });
+    }
 });
 (function($) {
     $.fn.tab = function(ayarlar) {
@@ -75,22 +134,25 @@ $("body").on("click", ".tr-karart.menu", function() {
     };
 })(jQuery);
 $(".tr-tab").tab();
-$(".tr-filtre [data-source=hepsi]").addClass("aktif");
+
+
+$(".tr-filtre [data-source='hepsi'], .filtre-icerik").addClass("aktif");
 $(".tr-filtre").on("click", "nav a", function() {
     var source = $(this).data("source");
-    $(this).addClass("aktif").siblings().removeClass("aktif").end().parents(".tr-filtre").find(".tr.icerik").removeClass("gizle").not("[data-target^=" + source + "]").addClass("gizle");
+    $(this).addClass("aktif").siblings().removeClass("aktif").parents(".tr-filtre").find(".filtre-icerik").addClass("aktif").not("[data-target^=" + source + "]").removeClass("aktif");
     if (source == "hepsi" || source == "hepsi aktif") {
-        $(this).parents(".tr-filtre").find(".icerik").removeClass("gizle");
+        $(this).parents(".tr-filtre").find(".filtre-icerik").addClass("aktif");
     }
 })
+
 $(".akordiyon-icerik").hide();
-$(".tr-akordiyon > a.baslik").on("click", function() {
+$(".tr-akordiyon > .baslik").on("click", function() {
     var t = $(this);
-    var i = t.closest(".tr-akordiyon").children("a.baslik").index(t);
+    var i = t.closest(".tr-akordiyon").children(".baslik").index(t);
     if (t.hasClass("aktif")) {
-        t.removeClass("aktif").closest(".tr-akordiyon").children(".akordiyon-icerik").slideUp(250);
+        t.removeClass("aktif").closest(".tr-akordiyon").children(".akordiyon-icerik").slideUp(100);
     } else {
-        t.siblings().removeClass("aktif").end().addClass("aktif").closest(".tr-akordiyon").children(".akordiyon-icerik").eq(i).slideDown(250).siblings(".akordiyon-icerik").slideUp(250);
+        t.siblings().removeClass("aktif").end().addClass("aktif").closest(".tr-akordiyon").children(".akordiyon-icerik").eq(i).slideDown(100).siblings(".akordiyon-icerik").slideUp(100);
     }
 })
 $("body").on("click", "a.modal", function(e) {
